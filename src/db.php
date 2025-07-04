e41e3ed47e74fdcc03ba0b8353e39290c874f304
caua-carvalho <caua.carma033@gmail.com>
2025-07-04 08:52:53 -0300

    permissao de comunicacao para o react

--- src/db.php
+++ src/db.php
@@ -1,5 +1,9 @@
 <?php
 // src/db.php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
 require __DIR__ . '/../config/config.php';
 
 try {
